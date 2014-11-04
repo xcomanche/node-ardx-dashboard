2fbde382c978d05299e75530a8d9a92516b4097f
Roman Shuvaryk <rshuv@rmn.com>
2014-11-04 18:30:26 +0200

    Installed angular and template

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodeArdxDashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nodeArdxDashboardApp
+ */
+angular.module('nodeArdxDashboardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
